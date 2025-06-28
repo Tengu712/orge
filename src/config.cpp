4871fbb84192132f345a68a97eb5436fe87f11ad
Tengu712 <genreihoutengu@gmail.com>
2025-06-28 16:49:17 +0900

    いったんコミット

--- src/config.cpp
+++ src/config.cpp
@@ -1,16 +1,56 @@
 #include "config.hpp"
 
+#include "graphics/platform.hpp"
+
+#include <unordered_map>
 #include <yaml-cpp/yaml.h>
 
-std::optional<Config> parse(const YAML::Node yaml) {
+Attachment parseAttachment(const YAML::Node &node, std::unordered_map<std::string, uint32_t> &attachmentMap) {
+	const auto id = node["id"].as<std::string>();
+	if (!attachmentMap.contains(id)) {
+		attachmentMap[id] = attachmentMap.size();
+	} else {
+		throw;
+	}
+	const auto format_ = node["format"].as<std::string>();
+	// TODO: parse format
+	const auto format = graphics::platform::getRenderTargetPixelFormat();
+	const auto discard = node["discard"].as<bool>(false);
+	const auto finalLayout_ = node["final-layout"].as<std::string>();
+	const auto finalLayout =
+		finalLayout_ == "general" ? vk::ImageLayout::eGeneral
+		: finalLayout_ == "color-attachment" ? vk::ImageLayout::eColorAttachmentOptimal
+		: finalLayout_ == "depth-stencil-attachment" ? vk::ImageLayout::eDepthStencilAttachmentOptimal
+		: finalLayout_ == "transfer-src" ? vk::ImageLayout::eTransferSrcOptimal
+		: finalLayout_ == "present-src" ? vk::ImageLayout::ePresentSrcKHR
+		: throw;
+	return {
+		format,
+		discard,
+		finalLayout,
+	};
+}
+
+Config parse(const YAML::Node yaml) {
 	const auto title = yaml["title"].as<std::string>();
 	const auto width = yaml["width"].as<int>();
 	const auto height = yaml["height"].as<int>();
-	return std::optional<Config>({
+
+	std::unordered_map<std::string, uint32_t> attachmentMap;
+	std::unordered_map<std::string, uint32_t> subpassMap;
+	std::vector<Attachment> attachments;
+	const auto attachments_ = yaml["attachments"];
+	std::vector<Subpass> subpasses;
+	std::vector<SubpassDependency> subpassDeps;
+
+	return {
 		title,
 		width,
 		height,
-	});
+		attachments,
+		subpasses,
+		subpassDeps,
+	};
 }
 
 std::optional<Config> parseConfig(const char *const yaml) {
--- src/config.hpp
+++ src/config.hpp
@@ -4,11 +4,38 @@
 
 #include <optional>
 #include <string>
+#include <vector>
+#include <vulkan/vulkan.hpp>
+
+struct Attachment {
+	const vk::Format format;
+	const bool discard;
+	const vk::ImageLayout finalLayout;
+};
+
+struct SubpassAttachment {
+	const uint32_t index;
+	const vk::ImageLayout layout;
+};
+
+struct Subpass {
+	const std::vector<SubpassAttachment> inputs;
+	const std::vector<SubpassAttachment> outputs;
+	const std::optional<SubpassAttachment> depth;
+};
+
+struct SubpassDependency {
+	const uint32_t src;
+	const uint32_t dst;
+};
 
 struct Config {
 	const std::string title;
 	const int width;
 	const int height;
+	const std::vector<Attachment> attachments;
+	const std::vector<Subpass> subpasses;
+	const std::vector<SubpassDependency> subpassDeps;
 };
 
 std::optional<Config> parseConfig(const char *const yaml);
