35e99f0abe9654a6f2f9780063eb4384cbf7928c
Tengu712 <genreihoutengu@gmail.com>
2025-06-30 02:49:09 +0900

    修正

--- cmake/merge_static_msvc.cmake
+++ cmake/merge_static_msvc.cmake
@@ -2,6 +2,11 @@
 #
 # NOTE: こうでもしないと統合できないので。
 
-string(REPLACE ";" " " ORGE_OBJECTS_SPACED "${ORGE_OBJECTS}")
+execute_process(
+	COMMAND lib /OUT:${BINARY_DIR}/orgestatic.lib ${SDL3_LIB} ${YAML_CPP_LIB} ${ORGE_OBJECTS}
+	RESULT_VARIABLE LIB_RESULT
+)
 
-execute_process(COMMAND lib /OUT:${BINARY_DIR}/orgestatic.lib ${SDL3_LIB} ${YAML_CPP_LIB} ${ORGE_OBJECTS_SPACED})
+if(NOT LIB_RESULT EQUAL 0)
+	message(FATAL_ERROR "Failed to create merged static library")
+endif()
