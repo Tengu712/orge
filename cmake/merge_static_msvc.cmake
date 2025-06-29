cfc0a650d170fc4d7d86e4a3de46afc0f84fcea6
Tengu712 <genreihoutengu@gmail.com>
2025-06-30 02:41:54 +0900

    修正

--- cmake/merge_static_msvc.cmake
+++ cmake/merge_static_msvc.cmake
@@ -2,6 +2,6 @@
 #
 # NOTE: こうでもしないと統合できないので。
 
-string(REPLACE ";" " " ORGE_OBJECTS_SPACED "${ORGE_OBJECTS}")                                                                                                                                     │ │
+string(REPLACE ";" " " ORGE_OBJECTS_SPACED "${ORGE_OBJECTS}")
 
 execute_process(COMMAND lib /OUT:${BINARY_DIR}/orgestatic.lib ${SDL3_LIB} ${YAML_CPP_LIB} ${ORGE_OBJECTS_SPACED})
