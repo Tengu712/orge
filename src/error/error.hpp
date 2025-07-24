3adc2f670e3d2baa8df6b310a91d83077ffcc209
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2025-07-24 17:07:28 +0900

    fix: TRYを移動

--- src/error/error.hpp
+++ src/error/error.hpp
@@ -1,30 +1,6 @@
 #pragma once
 
-#include <vulkan/vulkan.hpp>
-
-#define TRY(n) \
-	try { \
-		n; \
-		return 1; \
-	} catch (const char *e) { \
-		error::setMessage(std::string(e)); \
-		return 0; \
-	} catch (const std::string &e) { \
-		error::setMessage(e); \
-		return 0; \
-	} catch (const vk::SystemError &e) { \
-		error::setMessage(std::string(e.what())); \
-		return 0; \
-	} catch (const vk::Result &e) { \
-		error::setMessage("vulkan result: " + std::to_string(static_cast<int64_t>(e))); \
-		return 0; \
-	} catch (const std::exception &e) { \
-		error::setMessage(e.what()); \
-		return 0; \
-	} catch (...) { \
-		error::setMessage("unbound error."); \
-		return 0; \
-    }
+#include <string>
 
 namespace error {
 
@@ -34,5 +10,7 @@ enum class SpecialError {
 };
 
 void setMessage(const std::string &e);
+
+const std::string &getMessage();
 
 } // namespace error
--- src/input/input.hpp
+++ src/input/input.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <orge.h>
+
 namespace input {
 
 void update();
+
+int32_t getState(OrgeScancode scancode);
 
 } // namespace input
