c33a6ee8c098ca919a8fce8606cc4db146838cc0
Tengu712 <genreihoutengu@gmail.com>
2025-07-28 10:50:48 +0900

    いったんCIを見る

--- meson/install_linux.py
+++ meson/install_linux.py
@@ -1,21 +1,27 @@
-import glob
 import shutil
 import subprocess
 import sys
 
 def install(src, dst):
-	for n in glob.glob(src):
-		print('Installing ' + n + ' to ' + dst)
-		shutil.copy(n, dst)
+	print('Installing ' + n + ' to ' + dst)
+	shutil.copy(n, dst)
 
 srcdir = sys.argv[1]
 dstdir = sys.argv[2]
 default_library = sys.argv[3]
+vulkan_version = sys.argv[4]
 
-install(srcdir + '/libvulkan.so.1.*', dstdir + '/libvulkan.so')
+vulkan_lib = 'libvulkan.so.' + vulkan_version
+
+install(srcdir + '/' + vulkan_lib, dstdir + '/libvulkan.so')
 
 if default_library == 'static':
 	install(srcdir + '/libSDL3.a', dstdir)
 	install(srcdir + '/libyaml-cpp.a', dstdir)
 
-# TODO: change ID.
+print('Change libvulkan.so ID to libvulkan.so')
+subprocess.run(['patchelf', '--set-soname', 'libvulkan.so', dstdir + '/libvulkan.so'])
+
+if default_library == 'shared':
+	print('Change liborge.so dependence from ' + vulkan_lib + ' to libvulkan.so')
+	subprocess.run(['patchelf', '--replace-needed', vulkan_lib, 'libvulkan.so', dstdir + '/liborge.so'])
---
+++ meson/install_windows.py
@@ -0,0 +1,19 @@
+import shutil
+import sys
+
+def install(src, dst):
+	print('Installing ' + n + ' to ' + dst)
+	shutil.copy(n, dst)
+
+srclibdir = sys.argv[1]
+srcbindir = sys.argv[2]
+dstlibdir = sys.argv[3]
+dstbindir = sys.argv[4]
+default_library = sys.argv[5]
+
+install(srclibdir + '/vulkan-1.lib', dstlibdir)
+install(srcbindir + '/vulkan-1.dll', dstbindir)
+
+if default_library == 'static':
+	install(srclibdir + '/SDL3.lib', dstlibdir)
+	install(srclibdir + '/yaml-cpp.lib', dstlibdir)
