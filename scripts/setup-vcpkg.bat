828c7e9abb2e01c79be3b160a365d2267137b378
Tengu712 <genreihoutengu@gmail.com>
2025-08-04 13:29:55 +0900

    refactor

--- scripts/setup-vcpkg.bat
+++ scripts/setup-vcpkg.bat
@@ -7,25 +7,25 @@ setlocal enabledelayedexpansion
 if not defined ERRORLEVEL set ERRORLEVEL=0
 
 if "%~1"=="" (
-    echo Usage: %~nx0 ^<static^|shared^>
-    exit /b 1
+		echo Usage: %~nx0 ^<static^|shared^>
+		exit /b 1
 )
 
 set "CLARG=%~1"
 if not "%CLARG%"=="static" if not "%CLARG%"=="shared" (
-    echo Usage: %~nx0 ^<static^|shared^>
-    exit /b 1
+		echo Usage: %~nx0 ^<static^|shared^>
+		exit /b 1
 )
 
 set "PROJROOT=%~dp0.."
 set "VCPKG_EXE=%PROJROOT%\vcpkg\vcpkg.exe"
 if not exist "%VCPKG_EXE%" (
-    echo [ info ] running %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
-    call %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
-    if !ERRORLEVEL! neq 0 (
-        echo [ error ] failed to call %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
-        exit /b !ERRORLEVEL!
-    )
+		echo [ info ] running %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
+		call %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
+		if !ERRORLEVEL! neq 0 (
+				echo [ error ] failed to call %PROJROOT%\vcpkg\bootstrap-vcpkg.bat
+				exit /b !ERRORLEVEL!
+		)
 )
 
 set "TRIPLET=custom-x64-windows-%CLARG%"
@@ -34,12 +34,12 @@ set "VCPKG_INSTALL_CMD=%VCPKG_EXE% install --overlay-triplets=.\triplets --tripl
 echo [ info ] running %VCPKG_INSTALL_CMD%
 %VCPKG_INSTALL_CMD%
 if !ERRORLEVEL! neq 0 (
-    echo [ error ] failed to run %VCPKG_INSTALL_CMD%
-    exit /b !ERRORLEVEL!
+		echo [ error ] failed to run %VCPKG_INSTALL_CMD%
+		exit /b !ERRORLEVEL!
 )
 if not exist "%INSTALL_DIR%" (
-    echo [ error ] %INSTALL_DIR% not generated.
-    exit /b 1
+		echo [ error ] %INSTALL_DIR% not generated.
+		exit /b 1
 )
 
 exit /b 0
