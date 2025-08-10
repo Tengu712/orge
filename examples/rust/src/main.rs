c971228ba4eb9e744184f9e98d790030ea858783
Tengu712 <genreihoutengu@gmail.com>
2025-08-10 22:00:22 +0900

    triangle

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -8,9 +8,30 @@ unsafe extern "C" {
     fn orgeInitializeWith(yamlFilePath: *const c_char) -> u8;
     fn orgeTerminate();
     fn orgeUpdate() -> u8;
+    fn orgeCreateMesh(
+        id: *const c_char,
+        vertexCount: u32,
+        vertices: *const f32,
+        indexCount: u32,
+        indices: *const u32,
+    ) -> u8;
     fn orgeBeginRender() -> u8;
+    fn orgeDraw(
+        pipelineId: *const c_char,
+        meshId: *const c_char,
+        instanceCount: u32,
+        instanceOffset: u32,
+    ) -> u8;
     fn orgeEndRender() -> u8;
 }
+
+const VERTICES: &[f32] = &[
+    // 左下
+    -1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, // 上
+    0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0, // 右下
+    1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+];
+const INDICES: &[u32] = &[0, 1, 2];
 
 fn show_error_dialog() {
     unsafe {
@@ -23,16 +44,38 @@ fn show_error_dialog() {
 }
 
 fn main() {
-    if unsafe { orgeInitializeWith(CString::new("config.yml").unwrap().as_ptr()) } == 0 {
+    if unsafe { orgeInitializeWith(CString::new("config.yml").unwrap().as_ptr()) == 0 } {
         show_error_dialog();
         panic!("failed to initialize orge.");
     }
+    if unsafe {
+        orgeCreateMesh(
+            CString::new("triangle").unwrap().as_ptr(),
+            VERTICES.len() as u32,
+            VERTICES.as_ptr(),
+            INDICES.len() as u32,
+            INDICES.as_ptr(),
+        ) == 0
+    } {
+        show_error_dialog();
+        panic!("failed to create a triangle.");
+    }
 
-    while unsafe { orgeUpdate() } != 0 {
-        if unsafe { orgeBeginRender() } == 0 {
+    while unsafe { orgeUpdate() != 0 } {
+        if unsafe { orgeBeginRender() == 0 } {
             eprintln!("failed to begin rendering.");
         }
-        if unsafe { orgeEndRender() } == 0 {
+        if unsafe {
+            orgeDraw(
+                CString::new("PL").unwrap().as_ptr(),
+                CString::new("triangle").unwrap().as_ptr(),
+                1,
+                0,
+            ) == 0
+        } {
+            eprintln!("failed to begin rendering.");
+        }
+        if unsafe { orgeEndRender() == 0 } {
             eprintln!("failed to end rendering.");
         }
     }
