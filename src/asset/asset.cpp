6a9e62198f9e48c828276a48d5b72e112930719f
Tengu712 <genreihoutengu@gmail.com>
2025-08-11 16:46:26 +0900

    initialize

--- src/asset/asset.cpp
+++ src/asset/asset.cpp
@@ -3,10 +3,14 @@
 #include <fstream>
 #include <memory>
 #include <openssl/evp.h>
+#include <vector>
 
 namespace asset {
 
+constexpr size_t KEY_SIZE = 32;
 constexpr size_t IV_SIZE = 16;
+
+std::vector<unsigned char> g_dat;
 
 std::vector<unsigned char> loadDatFile() {
 	std::ifstream file(".dat", std::ios::binary);
@@ -24,15 +28,24 @@ std::vector<unsigned char> loadDatFile() {
 	return buffer;
 }
 
-std::vector<unsigned char> decryptDatFile(
-	const std::vector<unsigned char> &dat,
-	const std::vector<unsigned char> &key
-) {
+std::vector<unsigned char> decryptDatFile(const std::vector<unsigned char> &dat, const std::string &passphrase) {
 	using CipherContext = std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)>;
 
 	if (dat.size() < IV_SIZE) {
 		throw ".dat is invalid.";
 	}
+
+	std::vector<unsigned char> key(KEY_SIZE);
+	EVP_BytesToKey(
+		EVP_aes_256_cbc(),
+		EVP_sha256(),
+		nullptr,
+		reinterpret_cast<const unsigned char *>(passphrase.c_str()),
+		passphrase.length(),
+		1,
+		key.data(),
+		nullptr
+	);
 
 	const std::vector<unsigned char> iv(dat.cbegin(), dat.cbegin() + IV_SIZE);
 	const std::vector<unsigned char> body(dat.cbegin() + IV_SIZE, dat.cend());
@@ -48,6 +61,10 @@ std::vector<unsigned char> decryptDatFile(
 
 	decrypted.resize(static_cast<size_t>(len + lenadd));
 	return decrypted;
+}
+
+void initialize(const std::string &passphrase) {
+	g_dat = decryptDatFile(loadDatFile(), passphrase);
 }
 
 } // namespace asset
