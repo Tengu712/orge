77bc881ae0e22e9c018da30be8025417d59bf39a
Tengu712 <genreihoutengu@gmail.com>
2025-08-13 01:01:41 +0900

    refactor

--- examples/triangle/main.cpp
+++ examples/triangle/main.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 #include <orge.h>
-#include <vector>
 
 #define TRY(n) \
 	if (!(n)) { \
 		std::cout << orgeGetErrorMessage() << std::endl; \
 		return 1; \
 	}
-
-const std::vector<float> VERTICES{
-	// 左下
-	-1.0f, 1.0f, 0.0f,
-	1.0f, 0.0f, 0.0f, 1.0f,
-	// 上
-	0.0f, -1.0f, 0.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-	// 右下
-	1.0f, 1.0f, 0.0f,
-	0.0f, 0.0f, 1.0f, 1.0f,
-};
-const std::vector<uint32_t> INDICES{0, 1, 2};
 
 int main() {
 	TRY(orgeInitialize());
