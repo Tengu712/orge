089a0957255439e9c48094af5297465593f4dd84
Tengu712 <genreihoutengu@gmail.com>
2025-08-30 19:15:58 +0900

    削除

--- scripts/install_linux.py
+++ scripts/install_linux.py
@@ -14,19 +14,7 @@ def install(src, dst):
 srcdir = sys.argv[1]
 dstdir = sys.argv[2]
 default_library = sys.argv[3]
-vulkan_version = sys.argv[4]
-
-vulkan_lib = 'libvulkan.so.' + vulkan_version
-
-install(srcdir + '/' + vulkan_lib, dstdir + '/libvulkan.so')
 
 if default_library == 'static':
 	install(srcdir + '/libSDL3.a', dstdir)
 	install(srcdir + '/libyaml-cpp.a', dstdir)
-
-print('Change libvulkan.so ID to libvulkan.so')
-subprocess.run(['patchelf', '--set-soname', 'libvulkan.so', dstdir + '/libvulkan.so'])
-
-if default_library == 'shared':
-	print('Change liborge.so dependence from ' + vulkan_lib + ' to libvulkan.so')
-	subprocess.run(['patchelf', '--replace-needed', vulkan_lib, 'libvulkan.so', dstdir + '/liborge.so'])
