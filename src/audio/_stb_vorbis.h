a7e60142414e479582ca78402ee7c1e06595393e
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2025-08-30 19:49:08 +0900

    fix: macOSのclangでビルドが失敗する問題を修正

--- src/audio/_stb_vorbis.h
+++ src/audio/_stb_vorbis.h
@@ -4,6 +4,10 @@
 // NOTE: MSVCで警告が出てビルドが失敗するので、stb_vorbis.cだけ例外的に警告を無効化する。
 #pragma warning(push)
 #pragma warning(disable : 4244 4245 4456 4457 4701)
+#elif defined(__clang__)
+// NOTE: Clangで警告が出てビルドが失敗するので、stb_vorbis.cだけ例外的に警告を無効化する。
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wtautological-compare"
 #endif
 
 #define STB_VORBIS_IMPLEMENTATION
@@ -11,4 +15,6 @@
 
 #ifdef _MSC_VER
 #pragma warning(pop)
+#elif defined(__clang__)
+#pragma clang diagnostic pop
 #endif
