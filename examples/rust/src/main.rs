e1547182c9333b1c5a902031b9f75d6b1b318115
Tengu712 <genreihoutengu@gmail.com>
2025-09-03 10:03:36 +0900

    rustは動いた

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -10,13 +10,12 @@ unsafe extern "C" {
     fn orgeUpdate() -> u8;
     fn orgeLoadMesh(id: *const c_char) -> u8;
     fn orgeBeginRender() -> u8;
-    fn orgeDraw(
-        pipelineId: *const c_char,
-        meshId: *const c_char,
-        instanceCount: u32,
-        instanceOffset: u32,
-    ) -> u8;
     fn orgeEndRender() -> u8;
+    fn orgeBindMesh(id: *const c_char) -> u8;
+    fn orgeBeginRenderPass(id: *const c_char) -> u8;
+    fn orgeEndRenderPass() -> u8;
+    fn orgeBindPipeline(id: *const c_char, indices: *const u32) -> u8;
+    fn orgeDraw(instanceCount: u32, instanceOffset: u32) -> u8;
 }
 
 fn show_error_dialog() {
@@ -36,22 +35,28 @@ fn main() {
     }
     if unsafe { orgeLoadMesh(CString::new("triangle").unwrap().as_ptr()) == 0 } {
         show_error_dialog();
-        panic!("failed to create a triangle.");
+        panic!("failed to load a triangle.");
     }
 
     while unsafe { orgeUpdate() != 0 } {
         if unsafe { orgeBeginRender() == 0 } {
             eprintln!("failed to begin rendering.");
         }
-        if unsafe {
-            orgeDraw(
-                CString::new("PL").unwrap().as_ptr(),
-                CString::new("triangle").unwrap().as_ptr(),
-                1,
-                0,
-            ) == 0
-        } {
-            eprintln!("failed to begin rendering.");
+        if unsafe { orgeBindMesh(CString::new("triangle").unwrap().as_ptr()) == 0 } {
+            eprintln!("failed to bind a mesh.");
+        }
+        if unsafe { orgeBeginRenderPass(CString::new("RP").unwrap().as_ptr()) == 0 } {
+            eprintln!("failed to begin a render pass.");
+        }
+        if unsafe { orgeBindPipeline(CString::new("PL").unwrap().as_ptr(), std::ptr::null()) == 0 }
+        {
+            eprintln!("failed to bind a pipeline.");
+        }
+        if unsafe { orgeDraw(1, 0) == 0 } {
+            eprintln!("failed to draw a mesh.");
+        }
+        if unsafe { orgeEndRenderPass() == 0 } {
+            eprintln!("failed to end a render pass.");
         }
         if unsafe { orgeEndRender() == 0 } {
             eprintln!("failed to end rendering.");
