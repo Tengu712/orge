6975578de57ec08a8d56a0b35c546c44688249d7
Tengu712 <genreihoutengu@gmail.com>
2025-09-15 16:34:46 +0900

    shared

--- src/graphics/resource/image-attachment.cpp
+++ src/graphics/resource/image-attachment.cpp
@@ -11,8 +11,8 @@
 
 namespace graphics::resource {
 
-// NOTE: MSVCだと謎のコンパイルエラーが発生するので、unique_ptrで管理する。
-std::vector<std::unordered_map<std::string, std::unique_ptr<Image>>> g_attachmentImages;
+// NOTE: MSVCでunique_ptrを含むunordered_mapのコピーエラーが発生するので、shared_ptrで管理する。
+std::vector<std::unordered_map<std::string, std::shared_ptr<Image>>> g_attachmentImages;
 
 void destroyAllAttachmentImages() noexcept {
 	g_attachmentImages.clear();
@@ -25,16 +25,16 @@ void initializeAllAttachmentImages() {
 	const auto &swapchain = window::swapchain();
 	const auto &images = swapchain.getImages();
 	const auto &extent = swapchain.getExtent();
+	g_attachmentImages.resize(images.size());
 	for (size_t i = 0; i < images.size(); ++i) {
-		g_attachmentImages.emplace_back();
 		for (const auto &[id, n]: config::config().attachments) {
 			const auto format = config::convertFormat(n.format, swapchain.getFormat());
 			const auto aspect = config::getImageAspectFromFormat(n.format);
 			if (n.format == config::Format::RenderTarget) {
-				g_attachmentImages[i].emplace(id, std::make_unique<Image>(images[i], format, aspect, 4));
+				g_attachmentImages[i].emplace(id, std::make_shared<Image>(images[i], format, aspect, 4));
 			} else {
 				const auto usage = config::getImageUsageFromFormat(n.format);
-				g_attachmentImages[i].emplace(id, std::make_unique<Image>(extent.width, extent.height, nullptr, format, usage, aspect, 4));
+				g_attachmentImages[i].emplace(id, std::make_shared<Image>(extent.width, extent.height, nullptr, format, usage, aspect, 4));
 			}
 		}
 	}
