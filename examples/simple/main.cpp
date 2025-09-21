e2c84c9e9d60fe705796a9ff8379ed66e4075e86
Tengu712 <genreihoutengu@gmail.com>
2025-09-21 21:08:22 +0900

    simple

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
-#include <orge.h>
+#include <orge/orge.h>
 
-#define TRY(n) \
-	if (!(n)) { \
-		std::cout << orgeGetErrorMessage() << std::endl; \
-		return 1; \
-	}
-
-#define CHECK(n) \
-	if (!(n)) { \
-		std::cout << orgeGetErrorMessage() << std::endl; \
-		continue; \
+#define CALL(type, param) \
+	{ \
+		const auto res = orgeApiCall(type, param); \
+		if (res == ORGE_ERROR) { \
+			orgeApiCall(ORGE_SHOW_ERROR_DIALOG, nullptr); \
+			return res; \
+		} \
 	}
 
 int main() {
-	TRY(orgeInitialize());
+	CALL(ORGE_INITIALIZE, nullptr);
+
+	OrgeBeginRenderPassParam beginRenderPassParam{"RP"};
 
 	int count = 0;
-	while (orgeUpdate()) {
-		CHECK(orgeBeginRender());
-		CHECK(orgeBeginRenderPass("RP"));
-		CHECK(orgeEndRenderPass());
-		CHECK(orgeEndRender());
-
+	while (orgeApiCall(ORGE_UPDATE, nullptr) != ORGE_WINDOW_CLOSED) {
+		CALL(ORGE_BEGIN_RENDER, nullptr);
+		CALL(ORGE_BEGIN_RENDER_PASS, &beginRenderPassParam);
+		CALL(ORGE_END_RENDER_PASS, nullptr);
+		CALL(ORGE_END_RENDER, nullptr);
 		if (count % 60 == 0) {
 			std::cout << count / 60 << std::endl;
 		}
 		count += 1;
 	}
 
-	orgeTerminate();
+	CALL(ORGE_TERMINATE, nullptr);
 	return 0;
 }
